59dea5f9257368802d89640688ab3d4d70b8ba64
johnny <johnnysilva11@hotmail.com>
2021-04-30 19:54:37 -0300

    Aplication todolist

---
+++ resources/js/app.js
@@ -0,0 +1,19 @@
+require('./bootstrap');
+
+import Vue from 'vue'
+
+import App from './vue/app'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPlusSquare, faTrash,faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faPlusSquare, faTrash, faUserSecret)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+const app = new Vue({
+    el: '#app',
+    component: { App },
+    render: h => h(App)
+});
